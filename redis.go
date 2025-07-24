49a727de12cb27bc53cccbce669755228830dfa7
Ryan <git@hexa.mozmail.com>
2025-07-23 21:20:34 -0400

    Refactor Redis backup functionality to use RESP format

--- redis.go
+++ redis.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"strconv"
@@ -20,16 +21,17 @@ func NewRedisBackup(config DatabaseConfig) *RedisBackup {
 	}
 }
 
-// Backup performs a Redis database backup using redis-cli --rdb
+// Backup performs a Redis database backup using redis-cli with RESP format
 func (r *RedisBackup) Backup(backupDir string) error {
 	// Create timestamp for the backup file
 	timestamp := time.Now().Format("2006-01-02-15-04-05")
-	backupFile := filepath.Join(backupDir, fmt.Sprintf("%s-%s.rdb", r.config.Database, timestamp))
+	backupFile := filepath.Join(backupDir, fmt.Sprintf("%s-%s.resp", r.config.Database, timestamp))
 
-	// Build redis-cli command arguments
+	// Build redis-cli command arguments for RESP export
 	args := []string{
 		"-h", r.config.Host,
 		"-p", strconv.Itoa(r.config.Port),
+		"--raw",
 	}
 
 	// Add authentication if password is provided
@@ -42,15 +44,62 @@ func (r *RedisBackup) Backup(backupDir string) error {
 		args = append(args, "-n", r.config.Database)
 	}
 
-	// Add the --rdb flag with output file
-	args = append(args, "--rdb", backupFile)
+	// Use a Lua script to export all keys and values as RESP commands
+	luaScript := `
+local keys = redis.call('keys', '*')
+local result = {}
+for i=1,#keys do
+    local key = keys[i]
+    local keytype = redis.call('type', key)['ok']
+    local ttl = redis.call('ttl', key)
+    
+    if keytype == 'string' then
+        local value = redis.call('get', key)
+        table.insert(result, '*3\r\n$3\r\nSET\r\n$' .. #key .. '\r\n' .. key .. '\r\n$' .. #value .. '\r\n' .. value .. '\r\n')
+    elseif keytype == 'hash' then
+        local hash = redis.call('hgetall', key)
+        local cmd = '*' .. (#hash + 2) .. '\r\n$5\r\nHMSET\r\n$' .. #key .. '\r\n' .. key .. '\r\n'
+        for j=1,#hash,2 do
+            cmd = cmd .. '$' .. #hash[j] .. '\r\n' .. hash[j] .. '\r\n$' .. #hash[j+1] .. '\r\n' .. hash[j+1] .. '\r\n'
+        end
+        table.insert(result, cmd)
+    elseif keytype == 'list' then
+        local list = redis.call('lrange', key, 0, -1)
+        for j=1,#list do
+            table.insert(result, '*3\r\n$5\r\nLPUSH\r\n$' .. #key .. '\r\n' .. key .. '\r\n$' .. #list[j] .. '\r\n' .. list[j] .. '\r\n')
+        end
+    elseif keytype == 'set' then
+        local set = redis.call('smembers', key)
+        for j=1,#set do
+            table.insert(result, '*3\r\n$4\r\nSADD\r\n$' .. #key .. '\r\n' .. key .. '\r\n$' .. #set[j] .. '\r\n' .. set[j] .. '\r\n')
+        end
+    elseif keytype == 'zset' then
+        local zset = redis.call('zrange', key, 0, -1, 'withscores')
+        for j=1,#zset,2 do
+            table.insert(result, '*4\r\n$4\r\nZADD\r\n$' .. #key .. '\r\n' .. key .. '\r\n$' .. #zset[j+1] .. '\r\n' .. zset[j+1] .. '\r\n$' .. #zset[j] .. '\r\n' .. zset[j] .. '\r\n')
+        end
+    end
+    
+    if ttl > 0 then
+        table.insert(result, '*3\r\n$6\r\nEXPIRE\r\n$' .. #key .. '\r\n' .. key .. '\r\n$' .. #tostring(ttl) .. '\r\n' .. tostring(ttl) .. '\r\n')
+    end
+end
+return table.concat(result)
+`
+	args = append(args, "--eval", luaScript, "0")
 
 	cmd := exec.Command("redis-cli", args...)
 
-	// Execute the backup
-	output, err := cmd.CombinedOutput()
+	// Execute the command and save output to file
+	output, err := cmd.Output()
 	if err != nil {
-		return fmt.Errorf("redis-cli --rdb failed: %v\nOutput: %s", err, string(output))
+		return fmt.Errorf("redis-cli eval failed: %v", err)
+	}
+
+	// Write the output to the backup file
+	err = os.WriteFile(backupFile, output, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write backup file: %v", err)
 	}
 
 	return nil
